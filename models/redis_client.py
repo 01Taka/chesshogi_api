cea117ca079c35b3517ad61901d77ff0c363ea10
01ou <Killkas01@gmail.com>
2025-01-24 23:46:11 +0900

    set requirements

--- models/redis_client.py
+++ models/redis_client.py
@@ -3,7 +3,7 @@ import redis
 import os
 from dotenv import load_dotenv
 
-# .envファイルをロード
+# .envファイルをロード（ローカル環境用）
 load_dotenv()
 
 def get_redis_client():
@@ -11,11 +11,15 @@ def get_redis_client():
     redis_host = os.getenv('REDIS_HOST', 'localhost')
     redis_port = int(os.getenv('REDIS_PORT', 6379))
     redis_password = os.getenv('REDIS_PASSWORD', None)
+    
+    # "true" / "false" を Python の bool に変換
+    redis_ssl = os.getenv('REDIS_SSL', 'true').lower() == 'true'
 
     # Redisクライアントを作成
     return redis.StrictRedis(
         host=redis_host,
         port=redis_port,
         password=redis_password,
+        ssl=redis_ssl,
         decode_responses=True
     )
